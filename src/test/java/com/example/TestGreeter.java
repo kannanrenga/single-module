012098026de66177df6feafb355ed3d5de5c9c32
Rengarajan kannan <ren.kan.76@gmail.com>
2023-06-18 23:22:21 +0400

    modified testgreeter.java

--- src/test/java/com/example/TestGreeter.java
+++ src/test/java/com/example/TestGreeter.java
@@ -41,4 +41,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
+ @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "God Vishnu help me";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
